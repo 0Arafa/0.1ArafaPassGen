c18851a0df22c6be494de00fee05a9ed876d86c7
0.1Arafa <86897733+0Arafa@users.noreply.github.com>
2022-01-29 10:42:11 +0200

    Update 0.1ArafaPassGen.py

--- 0.1ArafaPassGen.py
+++ 0.1ArafaPassGen.py
@@ -4,10 +4,10 @@ from string import ascii_lowercase,ascii_uppercase
 from random import shuffle,choices
 from os import system
 
-
 system("clear"),print("""#####    Simple and Powerful Password Generator    #####
 	       ##    By: Abd Almoen Arafa    ##
 	       ##    Age: 15    	     ##\n""")
+
 def content():
 	l_l,l_u,nums,syms=ascii_lowercase,ascii_uppercase,"0123456789","!@#$%^&*()_+-=[]{};:'|\/,.<>?"
 	list=[l_l,l_u,nums,syms]
@@ -21,6 +21,7 @@ def content():
 		print(List),print("\n","\t"*3,"#"*31,"\n")
 	except ValueError:	print("Sorry,You have to write just a numbers here\n"),content()
 	except KeyboardInterrupt:	print("\nBye ;)\n")
+
 content()
 #By Abd Almoen Arafa
 #I'm 15 years old
